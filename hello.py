573aac54c5f1e9a2a11686ca24ed9cbaae30e7f7
Gabe Durand <gdurand@jlgarchitects.com>
2024-03-11 21:34:32 -0500

    template

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
+from flask import url_for
 from flask import Flask
-from markupsafe import escape
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
